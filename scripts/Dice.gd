7ab96efaac74485224efb9c70326fe8e5a22f742
crisadoicinf <cristian.icinf@gmail.com>
2022-12-28 16:03:28 +0000

    improve dice

--- scripts/Dice.gd
+++ scripts/Dice.gd
@@ -1,37 +1,44 @@
 extends Node2D
 
-onready var dice: AnimatedSprite = $AnimatedSprite
-var textures: Array = []
-var totalTextures: Array = []
-var animation = "roll"
-var frames: SpriteFrames
+signal roll_finished()
 
+onready var sprite: Sprite = $Sprite
+onready var timer: Timer = $Timer
+var textures =[
+	preload("res://resources/dice/1.png"),
+	preload("res://resources/dice/2.png"), 
+	preload("res://resources/dice/3.png"), 
+	preload("res://resources/dice/4.png"), 
+	preload("res://resources/dice/5.png"), 
+	preload("res://resources/dice/6.png")
+]
+var time = 0
+var totalTime = 1
+var rollNumber;
 
 func _ready():
-	frames = dice.get_sprite_frames()
-	frames.set_animation_loop(animation, false)
-	var total = frames.get_frame_count(animation)
-	for i in range(total):
-		textures.append(frames.get_frame(animation, i))
-	totalTextures.append_array(textures)
-	totalTextures.append_array(textures)
-
-
+	randomize()
+	
+func set_number(number:int):
+	sprite.set_texture(textures[number -1])
+	
 func roll(number: int):
-	totalTextures.shuffle()
-	totalTextures.pop_back()
-	totalTextures.append(textures[number - 1])
-	frames.clear(animation)
-	for texture in totalTextures:
-		frames.add_frame(animation, texture)
-	dice.set_frame(0)
-	dice.play("roll")
-	yield(dice, "animation_finished")
-	dice.stop()
+	rollNumber = number
+	time = 0
+	timer.start()
+	yield(self, "roll_finished")
 
+func _on_timer_timeout():
+	time += timer.get_wait_time()
+	if(time <totalTime):
+		set_number(get_random_number())
+	else:
+		timer.stop()
+		set_number(rollNumber)
+		emit_signal("roll_finished")
 
 func is_rolling() -> bool:
-	return dice.is_playing()
+	return !timer.is_stopped()
 
 
 func get_random_number() -> int:
@@ -39,4 +46,5 @@ func get_random_number() -> int:
 
 
 func get_size() -> Vector2:
-	return frames.get_frame(animation, dice.get_frame()).get_size()
+	return sprite.texture.get_size() * sprite.get_scale()
+
